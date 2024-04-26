a8d1220e64b84c7d94e97ccb3c486bae298644db
Ілля Овсянніков <il.ovsiannikov@fora.ua>
2024-04-26 16:56:03 +0300

    added surprize

--- js/index.header.partial.js
+++ js/index.header.partial.js
@@ -1,12 +1,31 @@
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key=${e.code}]`);
-  const key = document.querySelectorAll(`.key[data-key=${e.code}]`);
+  if (finalMusicsPlayed) return;
 
+  const audio = document.querySelector(`audio[data-key=${e.code}]`);
   if (!audio) return;
 
+  const keys = document.querySelectorAll(`.key[data-key=${e.code}]`);
   audio.volume = 0.5;
   audio.play();
-  key.forEach((element) => element.classList.add("playing"));
+  keys.forEach((key) => key.classList.add("playing"));
+
+  if (allLettersWithPlayingClass()) {
+    setTimeout(() => {
+      finalMusicsPlayed = true;
+      const finalMusicHtml = document.querySelector(".finalMusic");
+      finalMusicHtml.volume = 0.5;
+      finalMusicHtml.play();
+    }, 2000);
+  }
 }
+
+function allLettersWithPlayingClass() {
+  const letters = document.querySelectorAll(`.header__keys .key`);
+  return Array.from(letters).every((letter) =>
+    letter.classList.contains("playing"),
+  );
+}
+
+let finalMusicsPlayed = false;
 
 window.addEventListener("keydown", playSound);
